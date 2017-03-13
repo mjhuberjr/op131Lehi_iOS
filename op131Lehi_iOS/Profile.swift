46fe3fe0ddd250fb01a8c8a5da2820b2a646a87c
Michael J Huber Jr <info@op131.com>
2017-03-13 12:27:07 -0600

    begin data model and receive image from URL

--- op131Lehi_iOS/Profile.swift
+++ op131Lehi_iOS/Profile.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017 operation thirteenOne. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+struct Profile {
+
+    // MARK: - Properties
+
+    var id: Int?
+
+    var givenName: String
+    var surname: String
+
+    var username: String
+    var password: String
+
+    var imagePath: String?
+    var image: UIImage? {
+        guard let imagePath = imagePath,
+            let url = URL(string: imagePath) else { return nil }
+
+        let data = dataFrom(imagePath: url)
+        let image = UIImage(data: data)
+
+        return image
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension Profile {
+    func dataFrom(imagePath: URL) throws -> Data {
+        let data = try Data(contentsOf: imagePath)
+        return data
+    }
+}
